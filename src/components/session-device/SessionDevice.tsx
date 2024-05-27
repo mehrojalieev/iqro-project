b49ce9722b19e7443d66262462dc7e624e7a6aec
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-05-27 10:17:26 +0500

    I completed Session device card

---
+++ src/components/session-device/SessionDevice.tsx
@@ -0,0 +1,32 @@
+import "./SessionDevice.scss"
+import Session from "../../assets/images/session.svg"
+
+const SessionDevice = () => {
+  return (
+    <div className="session-device">
+        <div className="session__device-header">
+            <h3>Sessions By Device</h3>
+            <p>year</p>
+        </div>
+        <div className="session-card">
+            <img className="session-image" src={Session} alt="Session Device" />
+            <div className="session__card-content">
+                    <div className="content-item">
+                        <p>Desktop</p>
+                        <h6>8,085<sub>13%</sub> </h6>
+                    </div>
+                    <div className="content-item">
+                        <p>Mobile</p>
+                        <h6>8,085<sub>30%</sub></h6>
+                    </div>
+                    <div className="content-item">
+                        <p>Tablets</p>
+                        <h6>8,085<sub>25%</sub> </h6>
+                    </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SessionDevice
