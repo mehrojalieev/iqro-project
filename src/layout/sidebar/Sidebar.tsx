2bb405ee651401cbbc1768c4fb30e3e5df405ea2
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-05-27 23:13:47 +0500

    I completed responsive for tablet

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,36 +1,49 @@
-import { Link, NavLink } from "react-router-dom"
+import {  NavLink } from "react-router-dom"
 import "./Sidebar.scss"
-import { HomeOutlined, MenuUnfoldOutlined } from "@ant-design/icons"
+import {  MenuUnfoldOutlined } from "@ant-design/icons"
+import { useState } from "react"
+
+
 
 const Sidebar = () => {
+
+  const [openSidebar, setOpenSidebar] = useState<boolean>(false)
+
   return (
-    <div className="sidebar">
+    <div  className={ openSidebar ? "sidebar sidebar-open" : "sidebar"}>
       <div className="sidebar-header">
-        <i className="header-icon"><MenuUnfoldOutlined /></i>
+        <i onClick={() => setOpenSidebar(!openSidebar)} className="header-icon"><MenuUnfoldOutlined /></i>
       </div>
       <ul className="sidebar-menu">
         <li>
           <NavLink className={({isActive}) => isActive ? "item-link item-link--active" : "item-link"} to={'home'}>
            <span className="material-symbols-outlined link-icon">home</span>
-            <p className="link-text">Home</p>
+            <p className="link-text">Manage All</p>
           </NavLink>
         </li>
         <li>
           <NavLink className={({isActive}) => isActive ? "item-link item-link--active" : "item-link"} to={'/s'}>
           <span className="material-symbols-outlined link-icon">space_dashboard</span>
-            <p className="link-text">Home</p>
+            <p className="link-text">Manage Products</p>
           </NavLink>
         </li>
         <li>
           <NavLink className={({isActive}) => isActive ? "item-link item-link--active" : "item-link"} to={'/a'}>
           <span className="material-symbols-outlined link-icon">trending_up</span>
-            <p className="link-text">Home</p>
+            <p className="link-text">Analytics</p>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink className={({isActive}) => isActive ? "item-link item-link--active" : "item-link"} to={'/a'}>
+          <span className="material-symbols-outlined link-icon">database</span>
+            <p className="link-text">Store</p>
           </NavLink>
         </li>
         <li>
           <NavLink className={({isActive}) => isActive ? "item-link item-link--active" : "item-link"} to={'/a'}>
-          <span className="material-symbols-outlined link-icon">database</span>
-            <p className="link-text">Home</p>
+          <span className="material-symbols-outlined link-icon">settings</span>
+            <p className="link-text">Settings</p>
           </NavLink>
         </li>
       </ul>
